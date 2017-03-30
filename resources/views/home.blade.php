815227236636f7ebf0beccbfe18eadb0f54476d5
muriel <murielthoms@gmail.com>
2017-03-30 04:12:00 +0200

    ajout citation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,23 @@
 @section('content')
 
 
-<!-- <div> -->
-	<img class="img" src="images/photo.jpg">
-	<div class="row">
-		<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-			<div class="site-heading">
-				<h1>MM</h1>
-				<hr class="small">
-				<span class="subheading">  « La connaissance est la seule chose qui s'accroit lorsqu'on la partage. »
-					Citation de Sacha Boudjema</span>
+
+<div class="photo container">
+	<div class="img">
+		<div class="row">
+			<div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+				<div class="site-heading">
+					<div class="mm">MM</div>
 				</div>
+				<span class="citation">« La connaissance est la seule chose qui s'accroit lorsqu'on la partage. »</span><br>
+				<h4 class="auteur">Sacha Boudjema.</h4>
 			</div>
 		</div>
-	<!-- </div> -->
+	</div>
+
+</div>
+@yield('content')
 
 
-	@endsection
+@endsection
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,51 +5,50 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>blog MM</title>
 </head>
-<body>
- <link href='http://fonts.googleapis.com/css?family=Lora' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Lora' rel='stylesheet' type='text/css'>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" >
+<link rel="stylesheet" type="text/css" href="css/app.css">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" >
+<link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/cinzel" type="text/css"/>.
 </head>
 
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="css/app.css">
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-
-
-
-<nav class=" menu navbar navbar-default navbar-fixed-top">
-  <div class="container">
-    <div class="navbar-header">
-      <img class="logo" src="images/logo.png">
-    </div> 
-    <div class=" social row">
-     <div class="col-md-4">
-       <a href="https://fr-fr.facebook.com/"><i class=" book fa fa-facebook"></i></a> 
-       <a href="https://twitter.com/?lang=fr"><i class="twit fa fa-twitter"></i></a> 
-       <a href="https://github.com/"><i class=" git fa fa-github" aria-hidden="true"></i></a>
-     </div>
-     <div class="link">
-       <i class=" h fa fa-flag-o" aria-hidden="true"></i>
-       <a class="home" href="/">Accueil</a>
-       <i class=" news fa fa-newspaper-o" aria-hidden="true"></i>
-       <a class="article" href="articles">Articles</a>
-       <i class=" o fa fa-user-o" aria-hidden="true"></i>
-       <a class="admin" href="admin">Administration</a>
-     </div>
+<body>
+  <nav class=" menu navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <img class="logo" src="images/logo.png">
+      </div>
+      <div class=" social row">
+       <div class="col-md-4">
+         <a href="https://fr-fr.facebook.com/"><i class=" book fa fa-facebook"></i></a> 
+         <a href="https://twitter.com/?lang=fr"><i class="twit fa fa-twitter"></i></a> 
+         <a href="https://github.com/"><i class=" git fa fa-github" aria-hidden="true"></i></a>
+       </div>
+       <div class="link">
+         <i class=" h fa fa-flag-o" aria-hidden="true"></i>
+         <a class="home" href="/">Accueil</a>
+         <i class=" news fa fa-newspaper-o" aria-hidden="true"></i>
+         <a class="article" href="articles">Articles</a>
+         <i class=" o fa fa-user-o" aria-hidden="true"></i>
+         <a class="admin" href="admin">Administration</a>
+       </div>
+     </div> 
    </div>
- </div>
-</nav>
+ </nav>
 
-@yield('content')
+ @yield('content')
 
-<div class="container">
+ <footer>
   <div class="row">
-    <p> Copyright<i class=" cop fa fa-copyright" aria-hidden="true"></i> BLOG MM 2017</p>
-  </div>
+     Copyright<i class=" cop fa fa-copyright" aria-hidden="true"></i> BLOG MM 2017
+  </footer>
 
-</div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"</script>
 
-</body>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"</script>
+
+ </body>
 </html>
 
 
