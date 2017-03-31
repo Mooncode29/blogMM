1364530175630958b43c2d3443ba415222e1aa7d
Mooncode29 <irene.ravelonarivo@gmail.com>
2017-03-31 11:17:19 +0200

    pb non resolu encore de l'affichage du css

--- resources/views/layout_admin.blade.php
+++ resources/views/layout_admin.blade.php
@@ -3,10 +3,10 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Admin area</title>
-	<link rel="stylesheet" href="{{ asset('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css')}}">
-	<link rel="stylesheet" href="{{ asset('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous') }}">
-	<link rel="stylesheet" type='text/css' href= "{{ asset('http://fonts.googleapis.com/css?family=Lora') }}">
-	<link rel="stylesheet" href="{{ asset('css/admin.css') }}">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" type='text/css' href= "http://fonts.googleapis.com/css?family=Lora">
+	<link rel="stylesheet" href="{!! ('public/css/admin.css') !!}">
 	
 </head>
 <body>
@@ -60,7 +60,10 @@
     </div>
     <!-- /#wrapper -->
 
-<script type="text/javascript" src="{{ asset('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js') }}"/>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"</script>
+<script type="text/javascript" src="{!! ('public/js/admin.js') !!}"></script>
 
 </body>
 </html>
