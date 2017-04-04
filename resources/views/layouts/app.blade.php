5846e2dedb3fadc478e50413e3152c9e12fe21ea
muriel <murielthoms@gmail.com>
2017-04-05 01:01:54 +0200

    remplacements de fichiers

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
 <head>
@@ -27,9 +28,9 @@
                      <!-- Collapsed Hamburger -->
                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
                         <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
+                        <span class="icon-bar">hyhyy</span>
+                        <span class="icon-bar">frfrf</span>
+                        <span class="icon-bar">dedede</span>
                     </button>
 
                     <!-- Branding Image -->
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('article', function(){
 });
 
 Route::get('master',function(){
-	return view('posts.create');
+	return view('posts.home');
 });
 
 Auth::routes();
